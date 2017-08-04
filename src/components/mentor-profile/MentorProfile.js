ca6594aa68661668e52bbaf687c6d9ac25492ab5
Janet Walker <janetwalker271989@gmail.com>
2017-08-04 19:58:43 -0400

    style

--- src/components/mentor-profile/MentorProfile.js
+++ src/components/mentor-profile/MentorProfile.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import '../../public/css/mentorConnection.css';
-import '../../public/css/bootstrap.css';
-import Mentor1 from '../../public/images/mentor1.jpg';
-import BeAMentor from '../../public/images/be-a-mentor.jpg';
+import '../../../public/css/mentorConnection.css';
+import '../../../public/css/bootstrap.css';
+
 
 
 
@@ -11,30 +10,9 @@ class MentorProfile extends Component {
   render() {
     return (
 
-      <div className="App">
-        
-        <div>
-          <img src={Mentor1} rounded alt="mentor" />
-        </div>
+      <div>
 
-        <div className="container">
-          <div className="row">
-            <h2 className="h2text">Tutorials</h2>
-            <h4 className="h4text">Select tutorials from a series of languages.</h4>
-            <Youtube />
-          </div>
-        </div>
-
-        <div className="container">
-          <div className="row">
-            <Books />
-          </div>
-        </div>
-
-        <div>
-          <img src={BeAMentor} className="img-style" alt="mentor" />
-        </div>
-
+       
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import MentorProfile from './components/mentor-profile/MentorProfile';
+
 
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
+);
+
+ReactDOM.render(
+  <MentorProfile />,
+  document.getElementById('mentorRoot')
 );
